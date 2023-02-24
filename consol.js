938ef6b0437796425e8e667dd98a892bd8c6891a
DileepTS <120468553+DileepTS@users.noreply.github.com>
2023-02-24 13:04:22 +0530

    Add files via upload

---
+++ consol.js
@@ -0,0 +1,76 @@
+//console.log(3-"A")
+//var a=10;
+  //  a=20;
+//console.log(a)
+
+//const a=20;
+//const a=30;
+//console.log(a);
+
+//console.log(1+"a");
+
+// var a=50;
+// let b=30;
+// const c=70;
+
+
+// console.log(window);
+
+
+
+
+
+// console.log(5+5);
+// console.log(5+"A");
+// console.log("ABC"+3);
+// console.log(6-3);
+// console.log(100-"Z");
+// console.log(100-"4");
+// console.log("6"-"3");
+// console.log(6-9);
+// console.log(5-7-"D");
+// console.log(3+9);
+// console.log("1");
+// alert("enter the value");
+// confirm("enter the value");
+// prompt("enter the value");
+// document.write("Hiiiiiii");
+// document.writeln("Byeeee");
+
+
+// document.writeln("Hohoho");
+// document.writeln("haaaaaa");
+
+// var a=5;
+// console.log("enter the value of a");
+// let b="Dilip";
+// console.log(b);
+
+
+// var a=Number(prompt("enter A"));
+// var b=Number(prompt("enter B"));
+// var c=a+b;
+// console.log(c);
+
+// var a=(prompt("enter A"));
+// var b=(prompt("enter B"));
+// var c=a+b;
+// console.log(c);
+
+
+// var str="Hello"
+// var str1='hello'
+// var str2=`hello`
+// console.log(str);
+// console.log(str1);
+// console.log(str2);
+
+// var empName="SAM";
+// var empId=143;
+// var empPhoneno=8574582;
+// console.log(`${empId} is the empId
+//                      ${empName} is the empName
+//               ${empPhoneno} is the empPhoneno`);
+
+
+
