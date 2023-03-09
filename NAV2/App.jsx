932185bbd2fbd88cbdc2e8241f4e2edfdcd1c13c
DileepTS <120468553+DileepTS@users.noreply.github.com>
2023-03-09 18:28:49 +0530

    Add files via upload

---
+++ NAV2/App.jsx
@@ -0,0 +1,17 @@
+import React from "react"
+import Nav2 from "./Nav2";
+import {Component} from "react"
+
+class App extends Component
+{
+    render()
+    {
+    return(
+        <>
+    
+     <Nav2 data="Hello"/>
+     </>
+    )
+    }
+}
+ export default App;
---
+++ NAV2/Nav2.jsx
@@ -0,0 +1,18 @@
+
+
+import React,{Component} from 'react';
+
+
+class Nav2 extends Component 
+{
+    render() {
+        return (
+            <div>
+              <h1>{this.props.data}</h1>  
+            </div>
+        );
+    }
+}
+
+export default Nav2;
+
