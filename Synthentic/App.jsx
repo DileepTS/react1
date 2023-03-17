6f91c0fe54471fea4c8406d6eabbd46b132c3ee6
DileepTS <120468553+DileepTS@users.noreply.github.com>
2023-03-17 13:46:07 +0530

    Add files via upload

---
+++ Synthentic/App.jsx
@@ -0,0 +1,32 @@
+import React, { Component } from 'react'
+
+export default class App extends Component {
+constructor()
+{
+super()
+this.state={
+  username:" "
+}
+}
+componentDidMount()
+{
+  let input=document.querySelector("input")
+  input.addEventListener("keypress",(e)=>{
+    this.setState({username:e.target.value})
+  })
+}
+  render() {
+    return (
+      <>
+      <div>{this.state.username}</div>
+      <input type="text"onChange={(e)=>{
+        this.setState({username:e.target.value})
+       }}></input>
+
+      </>
+    )
+  }
+}
+
+
+
