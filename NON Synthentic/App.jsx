ddb2c20dc18dc106b1c8bb6cb44963aa908a8990
DileepTS <120468553+DileepTS@users.noreply.github.com>
2023-03-17 13:22:05 +0530

    Add files via upload

---
+++ NON Synthentic/App.jsx
@@ -0,0 +1,29 @@
+import React, { Component } from 'react'
+
+export default class App extends Component {
+constructor()
+{
+super()
+this.state={
+  username:"Upendra"
+}
+}
+componentDidMount()
+{
+  let btn=document.querySelector("button")
+  btn.addEventListener("click",()=>{
+    this.setState({username:this.state.username="KGF"})
+  })
+}
+  render() {
+    return (
+      <>
+      <div>{this.state.username}</div>
+      <button>CHANGE</button>
+      </>
+    )
+  }
+}
+
+
+
